239c1b2b3d64a42b2b0547e4ec42ca4f92b3d1cf
yazanealaily <yazane.alaily@gmail.com>
2016-01-06 20:12:52 +0200

    add controller

---
+++ app.js
@@ -0,0 +1,12 @@
+angular.module("myApp", [])
+	.controller("madLibs", function($scope){
+		$scope.femaleName = "[ female name ]";
+		$scope.jobTitle = "[ job title ]";
+		$scope.tedTask = "[ tedious task ]";
+		$scope.dirTask = "[ dirty task ]";
+		$scope.celebrity = "[ celebrity ]";
+		$scope.uselessSkill = "[ useless skill ]";
+		$scope.obnCelebrity = "[ obnoxious celebrity ]";
+		$scope.number = "[ huge number ]";
+		$scope.adjective = "[ adjective ]";
+	});
