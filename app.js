989fa995135df1c33d78a8bab51f50b163f49345
yazanealaily <yazane.alaily@gmail.com>
2016-01-11 19:43:38 +0200

    add animation

---
+++ app.js
@@ -0,0 +1,31 @@
+angular.module("myApp", ["ngMessages", "ngAnimate"])
+	.controller("madLibs", function($scope){
+		
+		$scope.femaleName = "[ female name ]";
+		$scope.jobTitle = "[ job title ]";
+		$scope.tedTask = "[ tedious task ]";
+		$scope.dirTask = "[ dirty task ]";
+		$scope.celebrity = "[ celebrity ]";
+		$scope.uselessSkill = "[ useless skill ]";
+		$scope.obnCelebrity = "[ obnoxious celebrity ]";
+		$scope.number = "[ huge number ]";
+		$scope.adjective = "[ adjective ]";
+
+		$scope.madLib = false;
+		$scope.again = true;
+
+		$scope.submit = function() {
+
+			if($scope.myForm.$valid) {	
+				$scope.madLib = true;
+				$scope.again = false;
+			}
+		};
+
+		$scope.startOver = function() {
+			$scope.madLib = false;
+			$scope.again = true;	
+		}
+
+
+	});
